cbc6858a4f18cbc99d1728967ea9266ce6c1cf60
seolhyelin <tosiltosilkk@gmail.com>
2023-04-24 22:32:05 +0900

    Feat: Websockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -7,6 +9,14 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("http://localhost:3000");
-app.listen(3000, handleListen);
+const handleListen = () =>
+  console.log("http://localhost:3000, ws://localhost:3000");
+
+// http 서버 위에 ws 서버를 만듦
+const server = http.createServer(app);
+// websocket 서버만 동작 시키려면 괄호 안에 안넣어도 됨
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
