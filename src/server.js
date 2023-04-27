3cbbcbc71df7c0728076697f35821855fb6f13a8
seolhyelin <tosiltosilkk@gmail.com>
2023-04-27 14:01:32 +0900

    Feat: Room Notifications

--- src/server.js
+++ src/server.js
@@ -25,6 +25,13 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome");
+    socket.on("disconnecting", () => {
+      socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    });
+    socket.on("new_message", (msg, room, done) => {
+      socket.to(room).emit("new_message", msg);
+      done();
+    });
   });
 });
 
